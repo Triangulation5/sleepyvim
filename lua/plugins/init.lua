38587cf0fbfd435552dc094f8681bf4d5fff7d1d
Triangulation5 <joshua.jomet@gmail.com>
2026-04-13 16:34:46 -0400

    Disabled Undotree and Vessel

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,21 +8,22 @@ vim.pack.add({
     { src = "https://github.com/rose-pine/neovim" },
     { src = "https://github.com/Saghen/blink.cmp" },
     { src = "https://github.com/rafamadriz/friendly-snippets" },
-    { src = "https://github.com/gcmt/vessel.nvim" },
-    { src = "https://github.com/mbbill/undotree" },
+
+    -- { src = "https://github.com/gcmt/vessel.nvim" },
+    -- { src = "https://github.com/mbbill/undotree" },
     { src = "https://github.com/xiaket/codeium.nvim" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/hrsh7th/nvim-cmp" },
     { src = "https://github.com/ellisonleao/gruvbox.nvim" },
 })
 
-require("vessel").setup({
-    create_commands = true,
-    commands = {
-        view_marks = "Marks",
-        view_jumps = "Jumps",
-        view_buffers = "Buffers"
-    }
-})
+-- require("vessel").setup({
+--    create_commands = true,
+--    commands = {
+--        view_marks = "Marks",
+--        view_jumps = "Jumps",
+--        view_buffers = "Buffers"
+--    }
+-- })
 
 require("mason").setup()

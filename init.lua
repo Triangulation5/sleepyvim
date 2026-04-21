3e4edd311f6290eb82d8c506e6e827e16565d8de
Triangulation5 <joshua.jomet@gmail.com>
2026-04-21 18:49:12 -0400

    Updated init.lua

--- init.lua
+++ init.lua
@@ -83,7 +83,8 @@ require("telescope").setup({
         selection_caret = "› ",
         borderchars = { "", "", "", "", "", "", "", "" },
         path_displays = { "smart" },
-        layout_config = { height = 100, width = 400, prompt_position = "top", preview_cutoff = 40 }
+        layout_config = { height = 100, width = 400, prompt_position = "top", preview_cutoff = 40 },
+        history = false,
     }
 })
 require("telescope").load_extension("ui-select")

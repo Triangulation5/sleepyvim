a5cde24363ebd6b60cdbfd8523060eec9c4db15a
Triangulation5 <joshua.jomet@gmail.com>
2026-05-17 17:14:07 -0400

    Updated configuration and themes

--- lua/triangulation/lazy/themes.lua
+++ lua/triangulation/lazy/themes.lua
@@ -2,20 +2,20 @@ return {
     {
         "vague-theme/vague.nvim",
         commit = "24cd29d",
-        -- priority = 1000
-        lazy = false,
+        lazy = true,
         config = function()
             require("vague")
-            vim.cmd.colorscheme("vague")
+            -- vim.cmd.colorscheme("vague")
         end
     },
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        lazy = true,
+        lazy = false,
         config = function()
             require("rose-pine").setup({ styles = { transparency = true } })
-            -- vim.cmd.colorscheme("rose-pine-moon")
+            vim.cmd.colorscheme("rose-pine-moon")
+            vim.opt.colorcolumn = "80"
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end
